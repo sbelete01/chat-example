f597064e512e313d5c9d8bf8fd2ae3ddc1cf8796
sbelete01 <sbelete01@gmail.com>
2021-11-19 22:48:25 -0800

    update

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
-const app = require('express')();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
-const port = process.env.PORT || 3000;
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', (socket) => {
-  socket.on('chat message', msg => {
+  socket.on('chat message', (msg) => {
     io.emit('chat message', msg);
   });
 });
 
-http.listen(port, () => {
-  console.log(`Socket.IO server running at http://localhost:${port}/`);
+server.listen(3000, () => {
+  console.log('listening on *:3000');
 });
